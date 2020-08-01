b20792a8ae04074b582bc3c32f7c13bbf7a946fc
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2020-08-01 11:41:34 -0500

    Some CSS

--- src/components/Poster.js
+++
@@ -1,9 +0,0 @@
-/* import React from 'react'
-
-const Poster = (props) => {
-    return <div className="single-poster">
-        <h1 className="movie-poster">{props.movie.poster}</h1>
-    </div>
-}
-
-export default Poster */
